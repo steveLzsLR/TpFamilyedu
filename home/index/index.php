c8a415388e179f9fed665ef35da435a5c316a7a8
Setenve <1226468643@qq.com>
2017-07-29 00:04:43 +0800

    修改一些配置，解决资源加载失败问题

--- home/index/index.php
+++ home/index/index.php
@@ -1,4 +1,8 @@
 <?php
-define("APP_PATH","../Apps/");
+define("WEB_SITE",'web.family.com');
 define("APP_DEBUG",true);
+define("IMG_URL",'public/image');
+define("CSS_URL",'../../public/css');
+define("JS_URL", 'public/js');
+define("APP_PATH","../Apps/");
 require '../../ThinkPHP/ThinkPHP.php';
